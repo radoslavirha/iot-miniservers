94f9770ac9a3de6e6decbc121c3b247d18b9dd57
Radoslav Irha <radoslav.irha@gmail.com>
2026-03-17 20:27:32 +0100

    CI bugs

---
+++ .changeset/many-singers-sniff.md
@@ -0,0 +1,7 @@
+---
+"@radoslavirha/miot-device": patch
+"interactive-map-feeder": patch
+"miot-bridge": patch
+---
+
+CI bugs
--- packages/miot-device/vitest.config.ts
+++ packages/miot-device/vitest.config.ts
@@ -1,4 +1,15 @@
 import { defaultConfig } from '@radoslavirha/config-vitest';
 import { defineConfig, mergeConfig } from 'vitest/config';
 
-export default defineConfig(mergeConfig(defaultConfig, {}));
+export default defineConfig(mergeConfig(defaultConfig, {
+    test: {
+        coverage: {
+            thresholds: {
+                branches: 94.44,
+                functions: 95,
+                lines: 95,
+                statements: 95
+            }
+        }
+    }
+}));
