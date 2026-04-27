ad4bd4dce898f4c51de49436e119101d72ac3dfe
Radoslav Irha <radoslav.irha@gmail.com>
2026-04-27 20:46:26 +0200

    fix logger class

--- apis/interactive-map-feeder-api/src/global/providers/LoggerProvider.ts
+++ apis/interactive-map-feeder-api/src/global/providers/LoggerProvider.ts
@@ -1,8 +1,7 @@
 import { Logger } from '@radoslavirha/tsed-logger';
-import { Injectable, OverrideProvider, ProviderScope, Scope } from '@tsed/di';
+import { OverrideProvider, ProviderScope, Scope } from '@tsed/di';
 import { ConfigService } from '../services/ConfigService.js';
 
-@Injectable()
 @OverrideProvider(Logger)
 @Scope(ProviderScope.SINGLETON)
 export class LoggerProvider extends Logger {
